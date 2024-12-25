0520369fdcc9d78ae5ef70076b6ab0a51a8d119c
Luiz Popoff <84781502+luizpopoff@users.noreply.github.com>
2024-12-25 20:07:56 -0300

    Update bronze_to_silver.py

--- notebooks/bronze_to_silver.py
+++ notebooks/bronze_to_silver.py
@@ -1,9 +1,8 @@
 # Databricks notebook source
-# MAGIC %sql
-# MAGIC select * from bronze.dataset_imoveis
 
 # COMMAND ----------
 
+# Consulta os dados armazenados na tabela 'dataset_imoveis' da camada bronze
 df = spark.sql(
 f"""
     select * from bronze.dataset_imoveis
@@ -11,37 +10,46 @@ f"""
 
 # COMMAND ----------
 
+# Exibe as colunas aninhadas do DataFrame, desestruturando as informações do campo 'anuncio' 
+# e de seu subcampo 'endereco' para visualização
 df.select('anuncio.*','anuncio.endereco.*').display()
 
 # COMMAND ----------
 
+# Cria um novo DataFrame contendo as informações detalhadas desestruturadas do campo 'anuncio' e 'endereco'
 df_detalhados = df.select('anuncio.*','anuncio.endereco.*')
 
 # COMMAND ----------
 
+# Exibe o DataFrame detalhado, removendo as colunas 'endereco' e 'caracteristicas' que não são necessárias
 df_detalhados.drop('endereco','caracteristicas').display()
 
 # COMMAND ----------
 
+# Atualiza o DataFrame, removendo as colunas 'endereco' e 'caracteristicas' permanentemente
 df_detalhados = df_detalhados.drop('endereco','caracteristicas')
 
 # COMMAND ----------
 
+# Define o esquema (camada Silver) e o nome da tabela que será criada
 schema ="silver"
 table = "dataset_imoveis"
 
+# Define o caminho de destino no Data Lake onde os dados transformados serão armazenados
 path_target = 'abfss://imoveis@datalakepopoff.dfs.core.windows.net/silver/dataset_imoveis'
 
 # COMMAND ----------
 
-# Gravar os dados no formato Delta
+# Cria o banco de dados 'silver' caso ele ainda não exista
 spark.sql(" CREATE DATABASE IF NOT EXISTS silver")
 
+# Grava o DataFrame transformado na camada Silver, utilizando o formato Delta
 df_detalhados.write \
     .format('delta') \
-    .mode('overwrite') \
-    .option('overwriteSchema', 'true') \
+    .mode('overwrite') \  # Sobrescreve os dados existentes na tabela, se houver
+    .option('overwriteSchema', 'true') \  # Atualiza o esquema, caso necessário
     .saveAsTable(f'{schema}.{table}')
+
 
 # COMMAND ----------
 
